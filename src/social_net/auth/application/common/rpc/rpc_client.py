4ce609a73126d5f95072975ed1f274009a7d65b4
Nurlybaev Asilbek <kabynks@gmail.com>
2025-05-25 18:54:25 +0500

    Delete src/social_net/auth/application/common/rpc directory

--- src/social_net/auth/application/common/rpc/rpc_client.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any, Dict
-
-class BaseRpcClient(ABC):
-    @abstractmethod
-    async def call(self, payload: Dict[str, Any]) -> Any:
-        pass
-
--- src/social_net/auth/application/common/rpc/rpc_server.py
+++
@@ -1,7 +0,0 @@
-from aio_pika.abc import AbstractIncomingMessage
-from abc import abstractmethod, ABC
-
-
-class BaseRpcServer(ABC):
-    @abstractmethod
-    async def handle(self, message: AbstractIncomingMessage) -> None: ...
